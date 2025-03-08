0887368773a155324efc514d36dd91cc4b37f56b
AmyPoon <amy0827poon@gmail.com>
2025-03-08 21:45:23 +0800

    Update

--- bookingroomsystem/src/App.vue
+++ bookingroomsystem/src/App.vue
@@ -1,12 +1,66 @@
 <script setup>
-import { RouterView, } from 'vue-router'
+import { RouterView } from 'vue-router'
+import { ref } from 'vue'
 
+const userName = ref('User Name') // Replace with actual user name logic
+const showMenu = ref(false)
 
+const toggleMenu = () => {
+  showMenu.value = !showMenu.value
+}
 </script>
 
 <template>
-
-  <RouterView />
+  <nav>
+    <ul>
+      <li><a href="#home">BookingRoomSystem</a></li>
+      <li style="float: right; position: relative;">
+        <a href="#more-options" @click.prevent="toggleMenu">&#9776;</a>
+        <ul :style="{ display: showMenu ? 'block' : 'none', position: 'absolute', right: '0', backgroundColor: '#333', listStyleType: 'none', padding: '0', margin: '0' }">
+          <li><a href="#profile">Profile</a></li>
+          <li><a href="#bookings">Bookings</a></li>
+          <li><a href="#booking-record">Booking Record</a></li>
+          <li><a href="#about-us">About us</a></li>
+          <li><a href="#logout">Logout</a></li>
+        </ul>
+      </li>
+      <li style="float: right;"><span>{{ userName }}</span></li>
+    </ul>
+  </nav>
+  <router-view />
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  background-color: #333;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0; 
+  width: 100%;
+  z-index: 1000; 
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+
+nav ul li {
+  float: left;
+}
+
+nav ul li a, nav ul li span {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+nav ul li a:hover, nav ul li span:hover {
+  background-color: #111;
+}
+</style>
--- bookingroomsystem/src/views/HomeView.vue
+++ bookingroomsystem/src/views/HomeView.vue
@@ -1,9 +1,48 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
+    <MenuBar /> 
     <TheWelcome />
+    <section>
+      <h1>Welcome to the Booking Room System</h1>
+      <p>This is the home page of our booking room system. Here you can find information about our services and how to book a room.</p>>
+    </section>
   </main>
 </template>
+
+<script>
+export default {
+  methods: {
+    bookRoom() {
+      // Add your booking logic here
+      alert('Booking room...');
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 0.5em;
+}
+
+p {
+  font-size: 1.2em;
+  margin-bottom: 1em;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 1em;
+  cursor: pointer;
+}
+</style>
