c7f99da47a0cc28743d0f06c25786cbe620b18b0
cyLee040923 <cylee040923@gmail.com>
2025-03-12 13:06:38 +0800

    Add filtering options at bookings page

--- bookingroomsystem/src/views/BookingsView.vue
+++ bookingroomsystem/src/views/BookingsView.vue
@@ -36,6 +36,35 @@ export default {
             { label: 'Room 2', value: 'room_2', checked: false }
         ]);
 
+        const capacityOptions = ref([
+            { label: 'Select All', value: 'select_all', checked: false },
+            { label: '≤5', value: 'below_5', checked: false },
+            { label: '≤10', value: 'below_10', checked: false }
+        ]);
+
+        const participantOptions = ref([
+            { label: 'Select All', value: 'select_all', checked: false },
+            { label: '≤5', value: 'below_5', checked: false },
+            { label: '≤10', value: 'below_10', checked: false },
+            { label: '≤20', value: 'below_20', checked: false },
+            { label: '20 or above', value: 'above_20', checked: false }
+        ]);
+
+        const priceOptions = ref([
+            { label: 'Select All', value: 'select_all', checked: false },
+            { label: '$0 - $100', value: 'below_100', checked: false },
+            { label: '$101 - $200', value: '101_to_200', checked: false },
+            { label: '$201 - $300', value: '201_to_300', checked: false },
+            { label: '$301 or above', value: 'above_301', checked: false }
+        ]);
+
+        const availabilityOptions = ref([
+            { label: 'Select All', value: 'select_all', checked: false },
+            { label: 'Low Availability', value: 'low_availability', checked: false },
+            { label: 'High Availability', value: 'high_availability', checked: false },
+        ]);
+
+
         const toggleRoomSelectAll = () => {
             const selectAllOption = roomOptions.value.find(option => option.value === 'select_all');
             const isChecked = selectAllOption.checked;
@@ -70,6 +99,75 @@ export default {
             }
         };
 
+        const toggleCapacitySelectAll = () => {
+            const selectAllOption = capacityOptions.value.find(option => option.value === 'select_all');
+            const isChecked = selectAllOption.checked;
+            capacityOptions.value.forEach(option => {
+                option.checked = isChecked;
+            });
+        };
+
+        const handleCapacityOptionChange = (option) => {
+            if (option.value === 'select_all') {
+                toggleCapacitySelectAll();
+            } else {
+                const allChecked = capacityOptions.value.every(opt => opt.value === 'select_all' || opt.checked);
+                capacityOptions.value.find(opt => opt.value === 'select_all').checked = allChecked;
+            }
+        };
+
+        const toggleParticipantSelectAll = () => {
+            const selectAllOption = participantOptions.value.find(option => option.value === 'select_all');
+            const isChecked = selectAllOption.checked;
+            participantOptions.value.forEach(option => {
+                option.checked = isChecked;
+            });
+        };
+
+        const handleParticipantOptionChange = (option) => {
+            if (option.value === 'select_all') {
+                toggleParticipantSelectAll();
+            } else {
+                const allChecked = participantOptions.value.every(opt => opt.value === 'select_all' || opt.checked);
+                participantOptions.value.find(opt => opt.value === 'select_all').checked = allChecked;
+            }
+        };
+
+        const togglePriceSelectAll = () => {
+            const selectAllOption = priceOptions.value.find(option => option.value === 'select_all');
+            const isChecked = selectAllOption.checked;
+            priceOptions.value.forEach(option => {
+                option.checked = isChecked;
+            });
+        };
+
+        const handlePriceOptionChange = (option) => {
+            if (option.value === 'select_all') {
+                togglePriceSelectAll();
+            } else {
+                const allChecked = priceOptions.value.every(opt => opt.value === 'select_all' || opt.checked);
+                priceOptions.value.find(opt => opt.value === 'select_all').checked = allChecked;
+            }
+        };
+
+        const toggleAvailabilitySelectAll = () => {
+            const selectAllOption = availabilityOptions.value.find(option => option.value === 'select_all');
+            const isChecked = selectAllOption.checked;
+            availabilityOptions.value.forEach(option => {
+                option.checked = isChecked;
+            });
+        };
+
+        const handleAvailabilityOptionChange = (option) => {
+            if (option.value === 'select_all') {
+                toggleAvailabilitySelectAll();
+            } else {
+                const allChecked = availabilityOptions.value.every(opt => opt.value === 'select_all' || opt.checked);
+                availabilityOptions.value.find(opt => opt.value === 'select_all').checked = allChecked;
+            }
+        };
+
+
         return {
             startDate,
             endDate,
@@ -78,9 +176,17 @@ export default {
             timeOptions,
             roomOptions,
             categoryOptions,
+            capacityOptions,
+            participantOptions,
+            priceOptions,
+            availabilityOptions,
             setTimeRange,
             handleRoomOptionChange,
             handleCategoryOptionChange,
+            handleCapacityOptionChange,
+            handleParticipantOptionChange,
+            handlePriceOptionChange,
+            handleAvailabilityOptionChange,
             showTimeOptions
         };
     }
@@ -121,7 +227,7 @@ export default {
         </div>
         <div class="row">
             <div class="col-2">
-                <label class="form-label">Room Options:</label>
+                <label class="form-label">Room Type Options:</label>
                 <div class="dropdown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
                         data-bs-toggle="dropdown" aria-expanded="false">
@@ -150,6 +256,82 @@ export default {
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" :id="option.value"
                                     v-model="option.checked" @change="handleCategoryOptionChange(option)">
+                                <label class="form-check-label" :for="option.value">{{ option.label }}</label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-2">
+                <label class="form-label">Capacity Options:</label>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonCapacity"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select Capacity Options
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonCapacity">
+                        <li v-for="option in capacityOptions" :key="option.value" class="dropdown-item">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" :id="option.value"
+                                    v-model="option.checked" @change="handleCapacityOptionChange(option)">
+                                <label class="form-check-label" :for="option.value">{{ option.label }}</label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-2">
+                <label class="form-label">Participant Options:</label>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonParticipant"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select Participant Options
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonParticipant">
+                        <li v-for="option in participantOptions" :key="option.value" class="dropdown-item">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" :id="option.value"
+                                    v-model="option.checked" @change="handleParticipantOptionChange(option)">
+                                <label class="form-check-label" :for="option.value">{{ option.label }}</label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-2">
+                <label class="form-label">Price Options:</label>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonPrice"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select Price Options
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonPrice">
+                        <li v-for="option in priceOptions" :key="option.value" class="dropdown-item">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" :id="option.value"
+                                    v-model="option.checked" @change="handlePriceOptionChange(option)">
+                                <label class="form-check-label" :for="option.value">{{ option.label }}</label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-2">
+                <label class="form-label">Availability Options:</label>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonAvailability"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select Availability Options
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonAvailability">
+                        <li v-for="option in availabilityOptions" :key="option.value" class="dropdown-item">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" :id="option.value"
+                                    v-model="option.checked" @change="handleAvailabilityOptionChange(option)">
                                 <label class="form-check-label" :for="option.value">{{ option.label }}</label>
                             </div>
                         </li>
