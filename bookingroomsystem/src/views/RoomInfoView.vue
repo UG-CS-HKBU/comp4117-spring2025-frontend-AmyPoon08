167ea56bb552692acee874bcbd099ca38f3a5b8c
cyLee040923 <cylee040923@gmail.com>
2025-03-15 19:26:06 +0800

    connect admin room info page and create new room

---
+++ bookingroomsystem/src/views/RoomInfoView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+const searchQuery = ref('');
+
+</script>
+
+<template>
+    <div class="row mt-3">
+        <div class="col-md-4">
+            <input 
+                type="text" 
+                v-model="searchQuery" 
+                class="form-control" 
+                placeholder="Search..."
+            />
+        </div>
+
+        <!-- Create new room -->
+        <div class="col-3 ms-auto">
+            <button class="btn btn-primary w-100">
+                <a href="/createRoom" class="text-white">Add</a>
+            </button>
+        </div>
+    </div>
+</template>
