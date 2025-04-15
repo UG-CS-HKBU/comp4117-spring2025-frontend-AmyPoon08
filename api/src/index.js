81c89f8d07782d3c23adbbd0c4fc6428163b3d7b
cyLee040923 <cylee040923@gmail.com>
2025-04-15 22:49:22 +0800

    try modify login api

--- api/src/index.js
+++ api/src/index.js
@@ -10,6 +10,23 @@ const fetch = require('node-fetch');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    // Add CORS headers
+    context.res = {
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Origin": "https://happy-bay-03dd73d00.6.azurestaticapps.net",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        }
+    };
+
+    // Handle OPTIONS request for CORS preflight
+    if (req.method === "OPTIONS") {
+        context.res.status = 204;
+        return;
+    }
+    
     try {
         const parsedUrl = new URL(req.url);
         const url = new URL(`https://xxx.azurewebsites.net${parsedUrl.pathname}${parsedUrl.search}`);
